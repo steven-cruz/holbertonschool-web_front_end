1338f16593cb41e956cbd6009ac4d20bcbd18903
steven-cruz <stevencruz.300008@gmail.com>
2021-02-16 22:41:01 -0500

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,18 @@
+// the welcomeMessage function is created
+function welcomeMessage(fullName){
+    // welcome alert more fullName
+    alert('Welcome ' + fullName);
+}
+
+// create a variable (guillaume) that calls (welcomeMessage) with the argument (Guillaume)
+guillaume = function(){
+    welcomeMessage("Guillaume");
+}
+
+alex = function(){
+    welcomeMessage("Alex");
+}
+
+fred = function(){
+    welcomeMessage("Fred");
+}
